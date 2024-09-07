244a04459293de2ef85161026a248163ab9f3d8e
Jordan <jordan.boyce@cyberlion.dev>
2024-09-07 13:48:14 -0700

    fixed table buttons and padding

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,10 +5,11 @@ import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withHashLocation()),
-    provideHttpClient()
+    provideHttpClient(),
   ],
 };
